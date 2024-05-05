73037e762bc3a816ece24a4366a0c675e4137ec3
twentyfive21 <tina.winnn@gmail.com>
2024-05-05 19:42:17 -0400

    userinterface phase 1 finished

--- car-dealership/src/main/java/com/pluralsight/DealershipFileManager.java
+++ car-dealership/src/main/java/com/pluralsight/DealershipFileManager.java
@@ -1,4 +1,12 @@
 package com.pluralsight;
 
 public class DealershipFileManager {
+
+    public Dealership getDealership(){
+        return null;
+    }
+
+    public void saveDealership(Dealership dealership){
+
+    }
 }
--- car-dealership/src/main/java/com/pluralsight/UserInterface.java
+++ car-dealership/src/main/java/com/pluralsight/UserInterface.java
@@ -1,4 +1,51 @@
 package com.pluralsight;
 
 public class UserInterface {
+
+    private Dealership dealership;
+
+    public UserInterface() {
+
+    }
+
+    public void display(){
+
+    }
+
+    public void processGetByPriceRequest(){
+
+    }
+
+    public void processGetByMakeModelRequest(){
+
+    }
+
+    public void processGetByYearRequest(){
+
+    }
+
+    public void processGetByColorRequest(){
+
+    }
+
+    public void processGetByMileageRequest(){
+
+    }
+
+    public void processGetByVehicleTypeRequest(){
+
+    }
+
+    public void processGetAllVehiclesRequest(){
+
+    }
+
+    public void processAddVehicleRequest(){
+
+    }
+
+    public void processRemoveVehicleRequest(){
+
+    }
+
 }
