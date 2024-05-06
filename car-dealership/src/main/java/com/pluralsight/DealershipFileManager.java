dfc0ff0ad4c5243164556286ca29ab45d543ed9e
twentyfive21 <tina.winnn@gmail.com>
2024-05-05 22:27:24 -0400

    finished phase two of workshop

--- car-dealership/src/main/java/com/pluralsight/DealershipFileManager.java
+++ car-dealership/src/main/java/com/pluralsight/DealershipFileManager.java
@@ -1,12 +1,63 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.regex.Pattern;
+
 public class DealershipFileManager {
 
     public Dealership getDealership(){
-        return null;
+        // Instantiate a Dealership object
+        Dealership dealership = null;
+        try{// create buf reader and file reader to read from file
+            BufferedReader bufReader = new BufferedReader(new FileReader("inventory.csv"));
+            // split the dealership info up to create dealership object
+            String[] firstLine = bufReader.readLine().split(Pattern.quote("|"));
+            // set dealership object attributes
+            dealership = new Dealership(firstLine[0],firstLine[1],firstLine[2]);
+            String input;
+            // Continuously reads lines from the BufferedReader until the end of file (null) is reached,
+            while((input = bufReader.readLine()) != null){
+                // split at pipe
+                String[] line = input.split(Pattern.quote("|"));
+                // create each vehicle object
+                Vehicle current = new Vehicle(Integer.parseInt(line[0]),
+                        Integer.parseInt(line[1]), line[2],line[3],line[4],
+                        line[5],Integer.parseInt(line[6]),Double.parseDouble(line[7]));
+                // add to array list each vehicle object
+                dealership.addVehicle(current);
+            }
+        // close reader
+        bufReader.close();
+        }catch(Exception e){
+            System.out.println("Error loading inventory");
+        }
+        return dealership;
     }
 
+    // overwrite the inventory file with current dealership information
     public void saveDealership(Dealership dealership){
-
+     try{// create new buf write to write to file
+         BufferedWriter bufWriter = new BufferedWriter(new FileWriter("inventory.csv"));
+         // write first line of dealership object
+         bufWriter.write(dealership.getName() + "|" + dealership.getAddress() + "|" + dealership.getPhone());
+         // ship new line for vehicles
+         bufWriter.newLine();
+         String pipe = "|";
+         // loop through array list and add each vehicle
+         for(Vehicle car : dealership.getAllVehicles()){
+             bufWriter.write(car.getVin()+ pipe +car.getYear()+ pipe + car.getMake() + pipe
+                     +car.getModel()+ pipe + car.getVehicleType()+ pipe + car.getColor()+ pipe +
+                     car.getOdometer()+ pipe + car.getPrice());
+             // new line so vehicles are not stacked on top of each other
+             bufWriter.newLine();
+         }
+         // close the writer
+         bufWriter.close();
+     }catch (Exception e){
+         System.out.println("Error saving new dealership info");
+     }
     }
 }
